c0c23a729102b70a85efaf6724d56cd9ceb2c28e
Roshni-Bala <roshni.bala@yahoo.com>
2022-12-04 20:15:30 +0530

    PCGspectrogram prediction results display

--- PCGSpec_Prediction.py
+++ PCGSpec_Prediction.py
@@ -3,8 +3,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-# returns a compiled model
-# identical to the previous one
 
 
 
@@ -12,6 +10,7 @@ def prediction_8_cluster(path, waveform_path):
         # code for prediction
 
         print("entered pcgspec_prediction file")
+# --------------------------- ADD THE MODEL PREDICTION CODE HERE WITH h5 FILE + i added the code snippet temporarily, feel free to delete ------
         #img1 = image.load_img(path)
         # model = load_model('my_model.h5')
         # img_array = image.img_to_array(img)
@@ -19,27 +18,29 @@ def prediction_8_cluster(path, waveform_path):
         # img_batch = np.expand_dims(img_array, axis=0)
         # prediction = model.predict(img1_batch)
 
+
+# ------------ temp value PLS CHANGE ONCE ADDING THE MODEL ------------------------------
         prediction = 1
 
+# ------------------ DISPLAYING THE RESULTS ON UI ---------------------------------
         # st.write("PREDICTION : "+str(val))
         st.text("-- PREDICTED RESULT FOR FILE " + waveform_path + " -- ")
         if(prediction == [-1]):
             st.text("SUBJECT PCG NORMAL")
         else:
             st.text("SUBJECT PCG ABNORMAL")
+
+
+
         st.text("-- ACTUAL RESULT FOR FILE " + waveform_path + " -- ")
         df_actual = pd.read_csv("REFERENCE.csv")
         x = waveform_path
-        #x = x.split(".")[0].split("/")[-1]
+ 
         x = x.split(".")[1].split(".")[0].split("/")[-1]
         print("X IS ")
         print(x)
         m = 2
-        # for i in range(408):
-        #     if df_actual.iloc[i, 0] == x:
-        #         print('x is found!!!!!')
-        #         m = df_actual.iloc[i, 1]
-        # print(m)
+
         z = df_actual[df_actual['file_name'].str.contains(x)]
         m = z['label']
         m = np.array(m)
