fa8b5c15c2592e27e9b30cb5074794b070895df4
Roshni-Bala <roshni.bala@yahoo.com>
2023-02-06 20:15:45 +0530

    seperated p1 p2 using tabs

--- app.py
+++ app.py
@@ -30,108 +30,110 @@ waveform_path = file_selector()
 
 # Import local Libraries
 sys.path.insert(0, os.path.dirname(os.getcwd()))
+tab1, tab2 = st.tabs(["Phase I", "Phase II"])
+with tab2:
+    st.header("PHASE II")
 
-st.title("PHASE II")
-
-
-st.title ("PHASE I (July 2022 - December 2022)")
-st.title("PCG Classification - Spectrogram Clustering")
-with st.form(key='my_form2'):
-    #waveform_path = None
-    try:
-        #waveform_path = file_selector()
-        if(len(waveform_path) != 0):
-            st.write('You selected `%s`' % waveform_path)
-            st.success('File successfully loaded!', icon="✅")
-        else:
-            raise TypeError
-           
-    except TypeError:
-        print('No file Entered!')
-
-    submit_button = st.form_submit_button(label='Predict!')
-    try:
-        pcg_spec_workflow(waveform_path)
-
-        # path = "./img-ui/spectrogram_sample.jpg" HAS THE IMAGE OF SPECTROGRAM - BEFORE CLUSTER
-        # path = "cluster_spec_img.jpg" HAS THE IMAGE OF 8 CLUSTERED K MEANS SPECTROGRAM 
-
-        pred_path = "cluster_spec_img.jpg"
-        prediction_8_cluster(pred_path, waveform_path)
-
-
-
-    except ValueError:
-            st.error('Please enter WAV File!')
-
-st.title('PCG CLASSIFICATION - BEST MODEL')
-with st.form(key='my_form1'):
-   # waveform_path = None
-    try:
-      #  waveform_path = file_selector()
-        if(waveform_path.split('.')[2] == 'mat' or waveform_path.split('.')[2] == 'MAT'):
-            print("68:",waveform_path.split('.')[2])
-            st.error('Enter a WAV file!')
-        if(len(waveform_path) != 0):
-            st.write('You selected `%s`' % waveform_path)
-            st.success('File successfully loaded!')
-        else:
-            raise TypeError
-    except TypeError:
-        st.error('No file entered')
-    submit_button = st.form_submit_button(label='Predict!')
-
-    if(waveform_path.split('.')[2] not in ' mat'): # or waveform_path.split('.')[2] != ' MAT'):
+with tab1:
+    st.header("PHASE I (July 2022 - December 2022)")
+        
+    st.title("PCG Classification - Spectrogram Clustering")
+    with st.form(key='my_form2'):
+        #waveform_path = None
         try:
-            pcg_workflow(waveform_path)
-        except EOFError:
-            st.error("Enter WAV File!")
-    # except EOFError:
-    #     st.error('Please enter WAV file!')
-    
+            #waveform_path = file_selector()
+            if(len(waveform_path) != 0):
+                st.write('You selected `%s`' % waveform_path)
+                st.success('File successfully loaded!', icon="✅")
+            else:
+                raise TypeError
+            
+        except TypeError:
+            print('No file Entered!')
+
+        submit_button = st.form_submit_button(label='Predict!')
+        try:
+            pcg_spec_workflow(waveform_path)
+
+            # path = "./img-ui/spectrogram_sample.jpg" HAS THE IMAGE OF SPECTROGRAM - BEFORE CLUSTER
+            # path = "cluster_spec_img.jpg" HAS THE IMAGE OF 8 CLUSTERED K MEANS SPECTROGRAM 
+
+            pred_path = "cluster_spec_img.jpg"
+            prediction_8_cluster(pred_path, waveform_path)
+
+
+
+        except ValueError:
+                st.error('Please enter WAV File!')
+
+    st.title('PCG CLASSIFICATION - BEST MODEL')
+    with st.form(key='my_form1'):
+    # waveform_path = None
+        try:
+        #  waveform_path = file_selector()
+            if(waveform_path.split('.')[2] == 'mat' or waveform_path.split('.')[2] == 'MAT'):
+                print("68:",waveform_path.split('.')[2])
+                st.error('Enter a WAV file!')
+            if(len(waveform_path) != 0):
+                st.write('You selected `%s`' % waveform_path)
+                st.success('File successfully loaded!')
+            else:
+                raise TypeError
+        except TypeError:
+            st.error('No file entered')
+        submit_button = st.form_submit_button(label='Predict!')
+
+        if(waveform_path.split('.')[2] not in ' mat'): # or waveform_path.split('.')[2] != ' MAT'):
+            try:
+                pcg_workflow(waveform_path)
+            except EOFError:
+                st.error("Enter WAV File!")
+        # except EOFError:
+        #     st.error('Please enter WAV file!')
+        
 
 
 
 
-st.title("ECG CLASSIFICATION - BEST MODEL")
-print("35:",waveform_path.split(".")[2])
-if(waveform_path.split(".")[2]== 'wav'):
-    waveform_path1 = waveform_path.replace('wav', 'mat')
-    print("**38:",waveform_path1)
-    if(os.path.exists(waveform_path1) == True):
-        waveform_path = waveform_path1
-        print("96: ",waveform_path)
-    else:
-        st.error('No corresponding MAT file found for this WAV file!')
-print("99: ", waveform_path)
-with st.form(key='my_form'):
-    #   waveform_path = st.file_uploader("Upload ECG file", type=["mat"])
-    #waveform_path = None
-    try:
-        #waveform_path = file_selector()
-        if(len(waveform_path) != 0):
-            st.write('You selected `%s`' % waveform_path)
-            st.success('File successfully loaded!', icon="✅")
+    st.title("ECG CLASSIFICATION - BEST MODEL")
+    print("35:",waveform_path.split(".")[2])
+    if(waveform_path.split(".")[2]== 'wav'):
+        waveform_path1 = waveform_path.replace('wav', 'mat')
+        print("**38:",waveform_path1)
+        if(os.path.exists(waveform_path1) == True):
+            waveform_path = waveform_path1
+            print("96: ",waveform_path)
         else:
-            raise TypeError
-            #raise MatReadError("Mat file appears to be empty")
-    except TypeError:
-        print('No file Entered!')
-    except ValueError:
-        print("Value Error - MAT FILE")
+            st.error('No corresponding MAT file found for this WAV file!')
+    print("99: ", waveform_path)
+    with st.form(key='my_form'):
+        #   waveform_path = st.file_uploader("Upload ECG file", type=["mat"])
+        #waveform_path = None
+        try:
+            #waveform_path = file_selector()
+            if(len(waveform_path) != 0):
+                st.write('You selected `%s`' % waveform_path)
+                st.success('File successfully loaded!', icon="✅")
+            else:
+                raise TypeError
+                #raise MatReadError("Mat file appears to be empty")
+        except TypeError:
+            print('No file Entered!')
+        except ValueError:
+            print("Value Error - MAT FILE")
 
-    submit_button = st.form_submit_button(label='Predict!')
-    try:
-        ecg_workflow(waveform_path)
-    except TypeError:
-        st.error('Please enter MAT File!')
-    except ValueError:
-        st.error('Entered file should be of MAT type!')
+        submit_button = st.form_submit_button(label='Predict!')
+        try:
+            ecg_workflow(waveform_path)
+        except TypeError:
+            st.error('Please enter MAT File!')
+        except ValueError:
+            st.error('Entered file should be of MAT type!')
 
 
 
 
 
 
-    #waveform_path = "C:/Users/Roshni/Downloads/A03035.mat"
-    # print(waveform_path)
+        #waveform_path = "C:/Users/Roshni/Downloads/A03035.mat"
+        # print(waveform_path)
